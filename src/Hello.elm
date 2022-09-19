233cfed70ab85d695ff5c7498d66b64921126608
ishidafuu <ishida@gmail.com>
2022-09-19 13:00:12 +0900

    json

--- src/Hello.elm
+++ src/Hello.elm
@@ -5,7 +5,12 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
+import Json.Decode as D exposing (Decoder)
 import Set exposing (Set)
+
+
+type alias Message =
+    { timestamp : Float, data : D.Value }
 
 
 main : Program () Model Msg
@@ -23,13 +28,21 @@ main =
 
 
 type alias Model =
-    { result : String
+    { input : String
+    , userState : UserState
     }
+
+
+type UserState
+    = Init
+    | Waiting
+    | Loaded User
+    | Failed Http.Error
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { result = "" }
+    ( Model "" Init
     , Cmd.none
     )
 
@@ -39,24 +52,36 @@ init _ =
 
 
 type Msg
-    = Click
-    | GotRepo (Result Http.Error String)
+    = Input String
+    | Send
+    | Receive (Result Http.Error User)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Click ->
-            ( model
-            , Http.get
-                { url = "https:api.github.com/repos/elm/core", expect = Http.expectString GotRepo }
+        Input newInput ->
+            ( { model | input = newInput }
+            , Cmd.none
             )
 
-        GotRepo (Ok repo) ->
-            ( { model | result = repo }, Cmd.none )
+        Send ->
+            ( { model | input = "", userState = Waiting }
+            , Http.get
+                { url = "https://api.github.com/users/" ++ model.input
+                , expect = Http.expectJson Receive userDecoder
+                }
+            )
 
-        GotRepo (Err error) ->
-            ( { model | result = Debug.toString error }, Cmd.none )
+        Receive (Ok user) ->
+            ( { model | userState = Loaded user }
+            , Cmd.none
+            )
+
+        Receive (Err e) ->
+            ( { model | userState = Failed e }
+            , Cmd.none
+            )
 
 
 
@@ -66,7 +91,73 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Click ] [ text "Get Repo" ]
-        , p [] [ text model.result ]
+        [ Html.form [ onSubmit Send ]
+            [ input
+                [ onInput Input
+                , autofocus True
+                , placeholder "Github name"
+                , value model.input
+                ]
+                []
+            , button
+                [ disabled
+                    ((model.userState == Waiting)
+                        || String.isEmpty (String.trim model.input)
+                    )
+                ]
+                [ text "Submit" ]
+            ]
+        , case model.userState of
+            Init ->
+                text ""
+
+            Waiting ->
+                text "Waiting.."
+
+            Loaded user ->
+                a
+                    [ href user.htmlUrl
+                    , target "_blank"
+                    ]
+                    [ img
+                        [ src user.avatarUrl
+                        , width 200
+                        ]
+                        []
+                    , div [] [ text user.name ]
+                    , div []
+                        [ case user.bio of
+                            Just bio ->
+                                text bio
+
+                            Nothing ->
+                                text ""
+                        ]
+                    ]
+
+            Failed error ->
+                div [] [ text (Debug.toString error) ]
         ]
 
+
+
+-- Data
+
+
+type alias User =
+    { login : String
+    , avatarUrl : String
+    , name : String
+    , htmlUrl : String
+    , bio : Maybe String
+    }
+
+
+userDecoder : Decoder User
+userDecoder =
+    D.map5 User
+        (D.field "login" D.string)
+        (D.field "avatar_url" D.string)
+        (D.field "name" D.string)
+        (D.field "html_url" D.string)
+        (D.maybe (D.field "avatar_url" D.string))
